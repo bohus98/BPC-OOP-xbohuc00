604e11337cae83e6561616219da563a41b7e5a86
bohus98 <58601109+bohus98@users.noreply.github.com>
2024-02-22 12:52:00 +0100

    Cvicenie c. 3

---
+++ CV03/Matrix.cs
@@ -0,0 +1,152 @@
+using System;
+
+public class Matrix
+{
+    private double[,] data;
+
+    public Matrix(double[,] values)
+    {
+        data = values;
+    }
+
+    public static Matrix operator +(Matrix m1, Matrix m2)
+    {
+        if (m1.data.GetLength(0) != m2.data.GetLength(0) || m1.data.GetLength(1) != m2.data.GetLength(1))
+            throw new ArgumentException("Matrices must be of the same size for addition.");
+
+        double[,] result = new double[m1.data.GetLength(0), m1.data.GetLength(1)];
+
+        for (int i = 0; i < m1.data.GetLength(0); i++)
+        {
+            for (int j = 0; j < m1.data.GetLength(1); j++)
+            {
+                result[i, j] = m1.data[i, j] + m2.data[i, j];
+            }
+        }
+
+        return new Matrix(result);
+    }
+
+    public static Matrix operator -(Matrix m1, Matrix m2)
+    {
+        if (m1.data.GetLength(0) != m2.data.GetLength(0) || m1.data.GetLength(1) != m2.data.GetLength(1))
+            throw new ArgumentException("Matrices must be of the same size for subtraction.");
+
+        double[,] result = new double[m1.data.GetLength(0), m1.data.GetLength(1)];
+
+        for (int i = 0; i < m1.data.GetLength(0); i++)
+        {
+            for (int j = 0; j < m1.data.GetLength(1); j++)
+            {
+                result[i, j] = m1.data[i, j] - m2.data[i, j];
+            }
+        }
+
+        return new Matrix(result);
+    }
+
+    public static Matrix operator *(Matrix m1, Matrix m2)
+    {
+        if (m1.data.GetLength(1) != m2.data.GetLength(0))
+            throw new ArgumentException("Number of columns in the first matrix must be equal to the number of rows in the second matrix for multiplication.");
+
+        double[,] result = new double[m1.data.GetLength(0), m2.data.GetLength(1)];
+
+        for (int i = 0; i < m1.data.GetLength(0); i++)
+        {
+            for (int j = 0; j < m2.data.GetLength(1); j++)
+            {
+                for (int k = 0; k < m1.data.GetLength(1); k++)
+                {
+                    result[i, j] += m1.data[i, k] * m2.data[k, j];
+                }
+            }
+        }
+
+        return new Matrix(result);
+    }
+
+    public static bool operator ==(Matrix m1, Matrix m2)
+    {
+        if (ReferenceEquals(m1, null))
+            return ReferenceEquals(m2, null);
+        if (ReferenceEquals(m2, null))
+            return false;
+
+        if (m1.data.GetLength(0) != m2.data.GetLength(0) || m1.data.GetLength(1) != m2.data.GetLength(1))
+            return false;
+
+        for (int i = 0; i < m1.data.GetLength(0); i++)
+        {
+            for (int j = 0; j < m1.data.GetLength(1); j++)
+            {
+                if (m1.data[i, j] != m2.data[i, j])
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static bool operator !=(Matrix m1, Matrix m2)
+    {
+        return !(m1 == m2);
+    }
+
+    public static Matrix operator -(Matrix m)
+    {
+        double[,] result = new double[m.data.GetLength(0), m.data.GetLength(1)];
+
+        for (int i = 0; i < m.data.GetLength(0); i++)
+        {
+            for (int j = 0; j < m.data.GetLength(1); j++)
+            {
+                result[i, j] = -m.data[i, j];
+            }
+        }
+
+        return new Matrix(result);
+    }
+
+    public override string ToString()
+    {
+        string result = "";
+
+        for (int i = 0; i < data.GetLength(0); i++)
+        {
+            for (int j = 0; j < data.GetLength(1); j++)
+            {
+                result += data[i, j] + " ";
+            }
+            result += "\n";
+        }
+
+        return result;
+    }
+
+    public double Determinant()
+    {
+        if (data.GetLength(0) != data.GetLength(1))
+            throw new InvalidOperationException("Matrix must be square for determinant calculation.");
+        if (data.GetLength(0) > 3)
+            throw new InvalidOperationException("Determinant calculation only supported for matrices up to size 3x3.");
+
+        if (data.GetLength(0) == 1)
+        {
+            return data[0, 0];
+        }
+        else if (data.GetLength(0) == 2)
+        {
+            return data[0, 0] * data[1, 1] - data[0, 1] * data[1, 0];
+        }
+        else // 3x3 matrix
+        {
+            return data[0, 0] * data[1, 1] * data[2, 2] +
+                   data[0, 1] * data[1, 2] * data[2, 0] +
+                   data[0, 2] * data[1, 0] * data[2, 1] -
+                   data[0, 2] * data[1, 1] * data[2, 0] -
+                   data[0, 1] * data[1, 0] * data[2, 2] -
+                   data[0, 0] * data[1, 2] * data[2, 1];
+        }
+    }
+}
---
+++ CV03/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[,] values1 = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        double[,] values2 = { { 9, 8, 7 }, { 6, 5, 4 }, { 3, 2, 1 } };
+        Matrix m1 = new Matrix(values1);
+        Matrix m2 = new Matrix(values2);
+
+        Console.WriteLine("Matrix 1:");
+        Console.WriteLine(m1);
+
+        Console.WriteLine("Matrix 2:");
+        Console.WriteLine(m2);
+
+        Console.WriteLine("Matrix 1 + Matrix 2:");
+        Console.WriteLine(m1 + m2);
+
+        Console.WriteLine("Matrix 1 - Matrix 2:");
+        Console.WriteLine(m1 - m2);
+
+        Console.WriteLine("Matrix 1 * Matrix 2:");
+        Console.WriteLine(m1 * m2);
+
+        Console.WriteLine("Determinant of Matrix 1: " + m1.Determinant());
+        Console.WriteLine("Determinant of Matrix 2: " + m2.Determinant());
+    }
+}
