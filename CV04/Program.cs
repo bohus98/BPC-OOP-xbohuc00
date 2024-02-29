b71625172ac193f78f2a3b7d0cd349d494249e59
bohus98 <bohus98@gmail.com>
2024-02-29 17:47:25 +0100

    CV4

---
+++ CV04/Program.cs
@@ -0,0 +1,27 @@
+﻿﻿using System;
+
+namespace cv04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string testText = "Toto je retezec predstavovany nekolika radky,\n"
+                             + "ktere jsou od sebe oddeleny znakem LF (Line Feed).\n"
+                             + "Je tu i nejaky ten vykricnik! Pro ucely testovani i otaznik?\n"
+                             + "Toto je jen zkratka zkr. ale ne konec vety. A toto je\n"
+                             + "posledni veta!";
+
+
+            StringStatistics s = new StringStatistics(testText);
+            Console.WriteLine("Pocet slov: {0}", s.NumberOfWords());
+            Console.WriteLine("Pocet riadkov: {0}", s.NumberOfLines());
+            Console.WriteLine("Pocet viet: {0}", s.NumberOfSentences());
+            Console.WriteLine("Najdlhsie slovo: {0}", string.Join(", ", s.LongestWords()));
+            Console.WriteLine("Najkradsie slovo: {0}", string.Join(", ", s.ShortestWords()));
+            Console.WriteLine("Najcastejsie slova: {0}", string.Join(", ", s.MostCommonWord()));
+            Console.WriteLine("Zoradene slova: {0}", string.Join(", ", s.AlphabeticallySortedWords()));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CV04/StringStatistics.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace cv04
+{
+    public class StringStatistics
+    {
+        public string Text { get; set; }
+
+        public StringStatistics(string text)
+        {
+            this.Text = text;
+        }
+
+        public int NumberOfWords()
+        {
+            char[] delimiters = { ' ', '\n' };
+            return Text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+
+        public int NumberOfLines()
+        {
+            return Text.Split('\n').Length;
+        }
+
+        public int NumberOfSentences()
+        {
+            var sentenceEndRegex = @"(?<=[.!?])\s+(?=\p{Lu})";
+
+            var sentences = Regex.Split(Text, sentenceEndRegex);
+
+            return sentences.Length;
+        }
+
+        public string[] LongestWords()
+        {
+            var words = Regex.Split(Text, @"\W+");
+            var maxLength = words.Max(word => word.Length);
+            return words.Where(word => word.Length == maxLength).ToArray();
+        }
+
+        public string[] ShortestWords()
+        {
+            var words = Regex.Split(Text, @"\W+");
+            var minLength = words.Where(word => !string.IsNullOrWhiteSpace(word)).Min(word => word.Length);
+            return words.Where(word => word.Length == minLength).ToArray();
+        }
+
+        public string MostCommonWord()
+        {
+            var words = Regex.Split(Text, @"\W+");
+            var mostCommonWord = words.GroupBy(word => word.ToLower())
+                                  .OrderByDescending(group => group.Count())
+                                  .Select(group => group.Key)
+                                  .FirstOrDefault();
+            return mostCommonWord;
+        }
+
+        public string[] AlphabeticallySortedWords()
+
+        {
+            var words = Regex.Split(Text, @"\W+");
+            Array.Sort(words, StringComparer.InvariantCultureIgnoreCase);
+            return words;
+        }
+    }
+}
