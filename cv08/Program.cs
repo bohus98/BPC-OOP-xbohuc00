d8ba672e483d0257d6f4f35f28b3311af83b9c5d
bohus98 <58601109+bohus98@users.noreply.github.com>
2024-03-28 15:34:28 +0100

    c08

---
+++ cv08/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace cv08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            CultureInfo.CurrentCulture = new CultureInfo("cs-CZ");
+            Console.WriteLine("Archive logic");
+            TemperatureArchive t = new TemperatureArchive();
+            t.Load();
+            Console.WriteLine(t.PrintTemperature());
+            Console.WriteLine(t.AverageMonthlyTemperature());
+            t.Calibration(-0.1);
+            Console.WriteLine("After calibration");
+            Console.WriteLine(t.PrintTemperature());
+            Console.WriteLine(t.findYearTemperature(2019));
+            Console.WriteLine(t.AverageYearTemperature());
+            Console.WriteLine(t.AverageMonthlyTemperature());
+            t.Save();
+            Console.WriteLine();
+            Console.ReadLine();
+
+
+        }
+    }
+}
